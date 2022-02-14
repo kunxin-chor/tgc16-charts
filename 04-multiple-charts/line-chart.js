694f2c3cc0632c7ed3413cd2f354db0d0cf50f82
kunxin-chor <chorkunxin@yahoo.com>
2022-02-14 05:26:52 +0000

    Added multiple charts and synchronized chart

--- 04-multiple-charts/line-chart.js
+++ 04-multiple-charts/line-chart.js
@@ -1,5 +1,4 @@
-const sightings = [10, 13,15,22,34,23,55,78,44,31];
-const temperature = [33,21,22,24,25,26,26,21,31,44];
+
 
 const lineChartOptions = {
     'chart': {
@@ -16,7 +15,9 @@ const lineChartOptions = {
             'data': temperature
         }
     ],
-    'xaxis':['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct']
+    'xaxis':{
+        'categories':['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct']
+    }
 }
 
 const lineChart = new ApexCharts(document.querySelector('#line-chart'),lineChartOptions);
